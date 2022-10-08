2d9b88f711cb939dd66187958ef628766e0b56f7
Czy <694559114@qq.com>
2022-10-08 13:13:54 +0800

    创建jsconfig，简化/src

--- esport-education-platform/vue.config.js
+++ esport-education-platform/vue.config.js
@@ -1,5 +1,12 @@
+const path = require('path')
+const resolve = dir => path.join(__dirname, dir)
+
 module.exports = {
     devServer: {
         proxy: 'http://localhost:8081'
+    },
+    chainWebpack: config => {
+        config.resolve.alias
+            .set('@', resolve('src'))
     }
 }
